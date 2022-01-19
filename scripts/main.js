b952f8bdcad22813b74a08b82154aec4a0fe8065
Mahindra Jayavaram <mahindrajayavaram@gmail.com>
2022-01-19 17:15:36 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,26 +19,39 @@ async function apiCall(url) {
 
 
 function appendArticles(articles, main) {
-console.log(articles)
+// console.log(articles)
 console.log(main)
+main.innerHTML=null;
     //add append logic here
     articles.forEach((e) => {
 
-        console.log(title, image, description)
+        //console.log(title, image, description)
         let div = document.createElement("div")
         // div.id = "img";
 
+        let div1 = document.createElement("div")
         let p= document.createElement("p")
-        p.textContent = e.title
+        p.textContent = e.content
         // console.log()
+        div1.append(p)
 
+        let img_div = document.createElement("div")
         let img = document.createElement("img");
         img.src =e.image
+        img_div.append(img)
 
+        let div2 = document.createElement("div")
         let des = document.createElement("p")
         des.textContent = e.description;
+        div2.append(des)
 
-        div.append(p, des, img);
+        div.append(div1,div2,img_div);
+
+        div.addEventListener ("click", function()
+        {
+            localStorage.setItem("clicked_item",JSON.stringify(e));
+            window.location.href = "news.html"
+        })
         main.append(div)
     });
 
